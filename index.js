c0cdcc68077b53c0f3930bb8bdd843acfe379112
tatoMa <Jason.ma1904@gmail.com>
2019-06-17 15:20:44 +1000

    user  model and schema added

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require('express')
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+require('./models/user')
+const User = mongoose.model('user')
+
 const app = express()
+app.use(express.json())
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
 
 var db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('DB is connected'))
+
+app.get('/', (req, res) => {
+  //   res.send(User.find())
+  User.find().then(users => {
+    console.log(users)
+  })
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`server started on ${process.env.PORT}`)
