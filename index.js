15cd779f035434c33fad3f2d79c71fbb4a419215
tatoMa <Jason.ma1904@gmail.com>
2019-06-17 14:33:28 +1000

    added mongoDB connection

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+
+var db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('DB is connected'))
 
 app.listen(process.env.PORT, () => {
   console.log(`server started on ${process.env.PORT}`)
